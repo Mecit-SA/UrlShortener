32e734a1f1c5bcc4481282cb5512a60f6c1de338
Mecit Sarıgüzel <mecitsariguzel@gmail.com>
2024-12-28 09:21:52 +0300

    enhanced ui

--- src/Web/ClientApp/src/app/url-shortener/url-shortener.component.ts
+++ src/Web/ClientApp/src/app/url-shortener/url-shortener.component.ts
@@ -10,18 +10,39 @@ export class UrlShortenerComponent {
 
   url: string;
   isShortening = false;
+  showResult = false;
+  result: string;
+  showCopiedNotification = false;
 
   constructor(private urlsClient: UrlsClient) { }
 
   shorten(): void {
 
     this.isShortening = true;
+    this.showResult = false;
 
     const command = new ShortenUrlCommand({ url: this.url });
 
     this.urlsClient.shortenUrl(command).subscribe((res) => {
+      this.result = res;
       this.isShortening = false;
-      alert('your url is: ' + res);
+      this.showResult = true;
     });
+  }
+
+  onCopyClick(): void {
+    navigator.clipboard.writeText(this.result).then(
+      () => {
+        this.showCopiedNotification = true;
+
+        setTimeout(() => this.showCopiedNotification = false, 3000);
+      },
+      (err) => console.error('Failed to copy: ', err)
+    );
+  }
+
+  onCloseResultClick(): void {
+    this.showResult = false;
+    this.result = '';
   }
 }
