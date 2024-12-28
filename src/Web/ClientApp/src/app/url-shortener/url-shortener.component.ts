f673e21a7838dbae7a00bb92978983dd30902917
Mecit Sarıgüzel <mecitsariguzel@gmail.com>
2024-12-28 13:27:22 +0300

    added error handling

--- src/Web/ClientApp/src/app/url-shortener/url-shortener.component.ts
+++ src/Web/ClientApp/src/app/url-shortener/url-shortener.component.ts
@@ -13,21 +13,45 @@ export class UrlShortenerComponent {
   showResult = false;
   result: string;
   showCopiedNotification = false;
+  showErrorNotification = false;
+  errorMessage: string;
 
   constructor(private urlsClient: UrlsClient) { }
 
+  resetAllProps(): void {
+    this.showResult = false;
+    this.result = '';
+    this.showCopiedNotification = false;
+    this.showErrorNotification = false;
+    this.errorMessage = '';
+  }
+
   shorten(): void {
 
+    this.resetAllProps();
+
     this.isShortening = true;
-    this.showResult = false;
 
     const command = new ShortenUrlCommand({ url: this.url });
 
-    this.urlsClient.shortenUrl(command).subscribe((res) => {
-      this.result = res;
-      this.isShortening = false;
-      this.showResult = true;
-    });
+    this.urlsClient.shortenUrl(command).subscribe(
+      {
+        next: (res) => {
+          this.result = res;
+          this.isShortening = false;
+          this.showResult = true;
+        },
+        error: (err) => {
+          console.error('Error occurred while shortening URL:', err);
+          this.isShortening = false;
+          this.showResult = false;
+
+          if (err.status == 400)
+            this.showError(this.handleValidationErrors(err));
+        }
+      }
+    );
+
   }
 
   onCopyClick(): void {
@@ -45,4 +69,36 @@ export class UrlShortenerComponent {
     this.showResult = false;
     this.result = '';
   }
+
+  showError(message: string): void {
+    this.errorMessage = message;
+    this.showErrorNotification = true;
+  }
+
+  onCloseErrorClick(): void {
+    this.showErrorNotification = false;
+    this.errorMessage = '';
+  }
+
+  handleValidationErrors(err: any): string {
+    try {
+      const errors = JSON.parse(err.response).errors;
+
+      if (!errors || typeof errors !== 'object') {
+        return "An unknown error occurred.";
+      }
+
+      // Extract and format errors
+      return Object.entries(errors)
+        .map(([key, messages]) => {
+          // Ensure `messages` is an array
+          const messageList = Array.isArray(messages) ? messages : [messages];
+          return `${key}: ${messageList.join(" | ")}`;
+        })
+        .join(" | ");
+    } catch (e) {
+      return "An error occurred while parsing the validation errors.";
+    }
+  }
+
 }
